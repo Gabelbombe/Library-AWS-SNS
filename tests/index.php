d9d53fcf3139f435567547e0b7aa0a2ed8bed265
ehime <dodomeki@gmail.com>
2015-05-26 13:26:01 -0700

    Reloaded composer for correct lock, starting testfile for debugging

--- tests/index.php
+++ tests/index.php
@@ -1,3 +1,14 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
+header('Content-type: text/plain');
 
+define  ('APP_HOME', getenv('APP_HOME'));
+require APP_HOME . '/vendor/autoload.php';
+
+$sns = New Wrappers\SimpleNotificationService\SNS(
+    getenv('KEY'), getenv('SECRET')
+);
+
+print_r($sns);
